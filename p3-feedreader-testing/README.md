c42127bdb68f33662de146c4f5d06ca75ccf3c82
supernova16 <supernova16f@gmail.com>
2018-04-08 20:36:35 +0800

    Add code

---
+++ p3-feedreader-testing/README.md
@@ -0,0 +1,31 @@
+# Test a RSS Feedreader with Jasmine
+
+The goal of this project was to use Jasmine to test a given Feedreader web application.
+
+## Test Suites
+
+RSS Feeds:
+
+- Test feeds are defined
+- Test feed URLs are defined and not empty
+- Test feed names are defined and not empty
+
+The Menu:
+
+- Test the menu element is hidden by default
+- Test menu icon appears on click, and hides when click again
+
+Initial RSS Entry:
+
+- Test at least one entry displays on load after async call
+
+RSS Entry Change:
+
+- Test RSS entry changes on menu select
+
+
+
+## References
+
+- [js判断输入字符串是否为空、空格、null总结](https://blog.csdn.net/youyou_yo/article/details/51506951)
+- [订阅阅读器测试异步请求出现超时错误的解决方案](https://discussions.youdaxue.com/t/topic/53490/3)
--- p3-feedreader-testing/jasmine/spec/feedreader.js
+++ p3-feedreader-testing/jasmine/spec/feedreader.js
@@ -7,7 +7,7 @@
  * 我们得保证在 DOM 准备好之前他们不会被运行。
  */
 $(function() {
-    /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
+    /* 这是我们第一个测试用例组 - 其中包含了一定数量的测试。这个用例的测试
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
     */
     describe('RSS Feeds', function() {
@@ -25,41 +25,108 @@ $(function() {
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
+        it('the url of all feed are defined and not null', function() {
+          allFeeds.forEach(function(feed) {
+            expect(feed.url).toEqual(jasmine.anything());
+            expect(feed.url).not.toBe('');
+
+          });
+        });
 
 
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
+        it('the name of all feed are defined and not null ', function() {
+          allFeeds.forEach(function(feed) {
+            //expect(feed.name).toBeDefined();
+            expect(feed.name).toEqual(jasmine.anything());//not undefined and null
+            expect(feed.name).not.toBe('');
+          });
+        });
     });
 
 
-    /* TODO: 写一个叫做 "The menu" 的测试用例 */
+    /* TODO: 写一个叫做 "The menu" 的测试用例组 */
+    describe('The memu', function() {
 
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
+        it('is hidden by default', function() {
+          expect($('.menu-hidden').length).not.toBe(0);
+        });
 
          /* TODO:
           * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
-          * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
+          * 测试应该包含两个 expectation ： 当点击图标的时候菜单是否显示，
           * 再次点击的时候是否隐藏。
           */
+         it('on click switch to visible', function() {
+           $('.menu-icon-link').trigger('click');
+           expect($('.menu-hidden').length).toBe(0);
 
-    /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
+           $('.menu-icon-link').trigger('click');
+           expect($('body').hasClass('menu-hidden')).toBe(true);//another express method
 
-        /* TODO:
-         * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
-         * 里面至少有一个 .entry 的元素。
-         *
-         * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
-         * 和异步的 done() 函数。
-         */
+         });
+    });
+
+
+    /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例组 */
+    describe('Initial Entries', function() {
+
+      /* TODO:
+       * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
+       * 里面至少有一个 .entry 的元素。
+       *
+       * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
+       * 和异步的 done() 函数。
+       */
+      beforeEach(function(done) {
+        loadFeed(0, done);
+      });
+
+      it('the .feed container has at least one .entry element', function() {
+        expect('.feed .entry'.length).toBeGreaterThan(0);
+      });
+    });
+
 
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
+    describe('New Feed Selection', function() {
 
-        /* TODO:
-         * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
-         * 记住，loadFeed() 函数是异步的。
-         */
+      /* TODO:
+       * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
+       * 记住，loadFeed() 函数是异步的。
+       */
+      var originalFeed;
+      var newFeed;
+
+      beforeEach(function(done) {
+        //window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
+        $('.feed').empty();
+        loadFeed(0, function() {
+          originalFeed = $('.feed').find("h2").text();
+
+          loadFeed(1, function() {
+            done();
+          });
+        });
+      },15000);//由于 http://cdc.tencent.com/feed/ 的延迟，对默认的超时时间设置大一些
+
+      it('content will be changed when load a new feed', function(done) {
+        newFeed = $('.feed').find("h2").text();
+        expect(originalFeed).not.toEqual(newFeed);
+        done();
+      });
+
+      afterEach(function() {
+        //reload first feed after testing
+            loadFeed(0);
+        });
+
+    });
 }());
