f0b19e0f50d9224e394ea71832bd8491ff785a3c
supernova16 <supernova16f@gmail.com>
2018-04-18 04:16:37 +0800

    Add something padding

---
+++ sideproject-cat-clicker/js/app_mvo.js
@@ -0,0 +1,40 @@
+/**
+ * DOM 全部加载完再执行这个函数
+ * MVO 模式构建
+ */
+$(function() {
+  //模型
+  let model = {
+    init
+  };
+
+  //控制器/章鱼
+  let octopus = {
+    init: function() {
+
+    },
+
+
+  };
+
+
+  //视图
+  let view = {
+    //初始化
+    init: function() {
+      this.catList = $('.cat-nav')；
+      let
+    },
+
+    //渲染
+    render: function() {
+
+    },
+
+    //
+
+  };
+
+  //写完 model、octopus、view 后，使用 octopus 进行初始化，即一开始的初始化
+  octopus.init();
+});
