9eb1083bca9b347b9f260f55d2bca94358ccc7cf
supernova16 <supernova16f@gmail.com>
2018-05-09 17:58:55 +0800

    Revert "fix bugs"
    This reverts commit 287f6b15e383411e44ed5b9691cf11186eaa2846.

--- p4-neighbourhood-map/js/app.js
+++ p4-neighbourhood-map/js/app.js
@@ -1,29 +1,58 @@
+let shops = [];
+const markers = [];
 
-Promise.all(initData()).then(function() {
-  let map,infoWindow;
-  const markers = [];
+function init() {
+  const map = new AMap.Map('map',{
+    zoom: 11,
+    center: [121.473658,31.230378]
+  });
+  getShops();
+  console.log(shops)//这里返回 空数组？是因为先执行了这个语句，再执行的 getShops 吗
+  shops.forEach(function(data){
+    console.log(data);//未执行？
+  })
+}
 
-  function initMap() {
-    map = new AMap.Map('map',{
-        zoom:12,
-        center: [121.473702, 31.23039 ]
-    });
+function getShops() {
+  fetch('data/michelin_shanghai.json')
+  .then(res => res.json())
+  .then(results => {
+    shops = results.michelin_shanghai;
+    for (shop of shops) {
+      getPlaceDetails(shop);
+      }
+  })
+  .catch(err => console.log(err));
 
-    places.forEach(function (data) {
-      //console.log(data);
-      let lngLat = new AMap.LngLat(Number(data.longitude),Number(data.latitude));
+  function getPlaceDetails(shop) {
+    fetch(`http://restapi.amap.com/v3/place/text?keywords=${shop.name}&key=c835e23dde6f8ec7b42c02127bbed0e4&city=shanghai&output=json&offset=20&page=1&extensions=all`
+    ).then(res => res.json())
+    .then(placeDetails => {
+      shop.addr = placeDetails.pois[0].address;
+      shop.location = placeDetails.pois[0].location;
+      shop.tel = placeDetails.pois[0].tel;
+      shop.id = placeDetails.pois[0].id;
+      shop.img = placeDetails.pois[0].photos[0].url;
+      shop.rating = placeDetails.pois[0].biz_ext.rating;
+      shop.cost = placeDetails.pois[0].biz_ext.cost;
 
-      let marker = new AMap.Marker({
-        position: lngLat,
-        title: data.name,
-        clickable: true,
+      let lnglat = shop.location.split(',');
+      shop.lng = parseFloat(lnglat[0]);
+      shop.lat = parseFloat(lnglat[1]);
+    })
+    .catch(err => console.log(err));
+  }
+}
+
+function createMarkers(shop) {
+    let lnglatx = new AMap.LngLat(shop.lng, shop.lat);
+    const marker = new AMap.Marker({
+        icon: "http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+        position: lnglatx,
         map: map
       });
+    markers.push(marker);
+}
 
-      markers.push(marker);
-      console.log(markers);
-    })
-  }
-
-  initMap();
-})
+init();
+//ko.appplyBindings(new AppViewModel());
