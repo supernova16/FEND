287f6b15e383411e44ed5b9691cf11186eaa2846
supernova16 <supernova16f@gmail.com>
2018-05-09 09:37:13 +0800

    fix bugs

--- p4-neighbourhood-map/js/app.js
+++ p4-neighbourhood-map/js/app.js
@@ -1,58 +1,29 @@
-let shops = [];
-const markers = [];
 
-function init() {
-  const map = new AMap.Map('map',{
-    zoom: 11,
-    center: [121.473658,31.230378]
-  });
-  getShops();
-  console.log(shops)//这里返回 空数组？是因为先执行了这个语句，再执行的 getShops 吗
-  shops.forEach(function(data){
-    console.log(data);//未执行？
-  })
-}
+Promise.all(initData()).then(function() {
+  let map,infoWindow;
+  const markers = [];
 
-function getShops() {
-  fetch('data/michelin_shanghai.json')
-  .then(res => res.json())
-  .then(results => {
-    shops = results.michelin_shanghai;
-    for (shop of shops) {
-      getPlaceDetails(shop);
-      }
-  })
-  .catch(err => console.log(err));
+  function initMap() {
+    map = new AMap.Map('map',{
+        zoom:12,
+        center: [121.473702, 31.23039 ]
+    });
 
-  function getPlaceDetails(shop) {
-    fetch(`http://restapi.amap.com/v3/place/text?keywords=${shop.name}&key=c835e23dde6f8ec7b42c02127bbed0e4&city=shanghai&output=json&offset=20&page=1&extensions=all`
-    ).then(res => res.json())
-    .then(placeDetails => {
-      shop.addr = placeDetails.pois[0].address;
-      shop.location = placeDetails.pois[0].location;
-      shop.tel = placeDetails.pois[0].tel;
-      shop.id = placeDetails.pois[0].id;
-      shop.img = placeDetails.pois[0].photos[0].url;
-      shop.rating = placeDetails.pois[0].biz_ext.rating;
-      shop.cost = placeDetails.pois[0].biz_ext.cost;
+    places.forEach(function (data) {
+      //console.log(data);
+      let lngLat = new AMap.LngLat(Number(data.longitude),Number(data.latitude));
 
-      let lnglat = shop.location.split(',');
-      shop.lng = parseFloat(lnglat[0]);
-      shop.lat = parseFloat(lnglat[1]);
-    })
-    .catch(err => console.log(err));
-  }
-}
-
-function createMarkers(shop) {
-    let lnglatx = new AMap.LngLat(shop.lng, shop.lat);
-    const marker = new AMap.Marker({
-        icon: "http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
-        position: lnglatx,
+      let marker = new AMap.Marker({
+        position: lngLat,
+        title: data.name,
+        clickable: true,
         map: map
       });
-    markers.push(marker);
-}
 
-init();
-//ko.appplyBindings(new AppViewModel());
+      markers.push(marker);
+      console.log(markers);
+    })
+  }
+
+  initMap();
+})
--- p4-neighbourhood-map/js/data.js
+++ p4-neighbourhood-map/js/data.js
@@ -1,4 +1,8 @@
+let places = [];
+
+
 function initData() {
+  let promiseArray =[];
   const names = ['唐阁','Ultraviolet','8 ½ Otto e Mezzo Bombana','喜粤8号','御宝轩',
   '乔尔‧卢布松美食坊','逸龙阁','雍福会','Bo Shanghai','大董餐饮（环贸iapm）','大董海参店 (越洋广场)',
   '大蔬无界 (外滩)','福和慧','苏浙总会','Jean Georges','金轩','老干杯','老正兴',
@@ -22,16 +26,46 @@ function initData() {
       stars = 1;
     }
 
-    getPlaceDetails(name);
-    michelinSHH.push(Object.assign({name,stars,addr,location,tel,id,img,rating,cost}));
-
+    michelinSHH.push(Object.assign({name,stars}));
 
   }
   //console.log(michelinSHH);
-  function writeData() {
+  /*
     fs.writeFileSync('data/michelin_shanghai.json',JSON.stringify(results));
+  }*/
+
+  places = michelinSHH;
+
+  for (place of places ) {
+    promiseArray.push(new Promise(function(resolve, reject) {
+      getPlaceDetails(place, resolve, reject);
+    }))
   }
 
-}
+  function getPlaceDetails(place, resolve, reject) {
+    fetch(`http://restapi.amap.com/v3/place/text?keywords=${place.name}&key=2466d94d165232a59816a964bcd21c41&city=shanghai&output=json&offset=20&page=1&extensions=all`
+    )
+    .then(res => res.json())
+    .then(placeDetails => {
+      place.addr = placeDetails.pois[0].address;
+      place.location = placeDetails.pois[0].location;
+      place.tel = placeDetails.pois[0].tel;
+      place.id = placeDetails.pois[0].id;
+      place.img = placeDetails.pois[0].photos[0].url;
+      place.rating = placeDetails.pois[0].biz_ext.rating;
+      place.cost = placeDetails.pois[0].biz_ext.cost;
 
-initData();
+      let lnglat = place.location.split(',');
+      place.longitude = lnglat[0];
+      place.latitude = lnglat[1];
+      resolve();
+    })
+    .catch(err => {
+      reject();
+      console.log(err)
+    });
+  }
+
+  return promiseArray;
+
+}
